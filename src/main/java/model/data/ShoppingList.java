b3c751850309cb656b57de69a09af05e8fe588ed
PaperPolit <pawel.glowacki.grzyb@gmail.com>
2021-05-20 18:28:00 +0200

    Initial commit on ShoppingList

--- src/main/java/model/data/ShoppingList.java
+++ src/main/java/model/data/ShoppingList.java
@@ -1,8 +1,49 @@
 package model.data;
 
+import model.exception.NotImplementedException;
+import model.splitter.Splitter;
+
 import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ShoppingList implements Serializable {
     private static final long serialVersionUID = 1L;
-    // TODO
+    private Map<Dish, Double> selectedDishes;
+    private Map<Ingredient, Double> selectedIngredients;
+
+    public Map<Dish, Double> getChosenDishes(){
+        throw new NotImplementedException();   
+    }
+
+    public Map<Ingredient, Double> getRequiredIngredients(){
+        throw new NotImplementedException();
+    }
+
+    public void splitRequiredIngredients(Splitter splitter){
+        throw new NotImplementedException();
+    }
+
+    public boolean addDish(Dish dish, double quantity){
+        throw new NotImplementedException();
+    }
+
+    public boolean deleteDish(Dish dish){
+        throw new NotImplementedException();
+    }
+
+    public boolean addIngredient(Ingredient ingredient, double quantity){
+        throw new NotImplementedException();
+    }
+
+    public boolean deleteIngredient(Ingredient ingredient){
+        throw new NotImplementedException();
+    }
+
+    public void exportRequiredIngredients(){
+        throw new NotImplementedException();
+    }
+
 }
--- src/test/java/model/data/ShoppingListTest.java
+++ src/test/java/model/data/ShoppingListTest.java
@@ -1,9 +1,59 @@
 package model.data;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.converter.JavaTimeConversionPattern;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShoppingListTest {
-    // TODO
+    private ShoppingList list;
+    private Map<Dish, Double> dishes;
+    private Map<Ingredient, Double> ingredients;
+
+    /*
+    @BeforeEach
+    public void setUp(){
+        dishes = new HashMap<>();
+        ingredients = new HashMap<>();
+
+        dishes.put(mock(Dish.class), 2.0);
+        dishes.put(mock(Dish.class), 4.0);
+
+        list = new ShoppingList();
+    }*/
+
+    @Test
+    public void addDishCheckIfAdded() {
+        var dishToAdd = mock(Dish.class);
+        list.addDish(dishToAdd, 2);
+        assertTrue(list.getChosenDishes().containsKey(dishToAdd));
+    }
+
+    @Test
+    public void addIngredientCheckIfAdded() {
+        var ingredientToAdd = mock(Ingredient.class);
+        list.addIngredient(ingredientToAdd, 2);
+        assertTrue(list.getRequiredIngredients().containsKey(ingredientToAdd));      
+    }
+
+    @Test
+    public void deleteDishCheckIfDeleted() {
+
+    }
+
+    
+
+    @Test
+    public void deleteIngredientCheckIfDeleted() {
+
+    }
+
 }
